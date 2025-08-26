338e97cffaaa613e12fabe560a89004372028862
seixasxbr <mateus.seixas@tidewise.io>
2025-08-26 11:56:43 -0300

    initial commit by rock-create-orogen

---
+++ sonar_oculus_m750dTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef sonar_oculus_m750d_TYPES_HPP
+#define sonar_oculus_m750d_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace sonar_oculus_m750d {
+}
+
+#endif
+
