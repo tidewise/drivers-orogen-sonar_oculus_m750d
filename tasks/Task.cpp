6915e351467559e49da83586d528a2252bb86416
seixasxbr <mateus.seixas@tidewise.io>
2025-09-04 18:12:10 -0300

    feat: add task

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -1,49 +1,76 @@
 /* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
 
 #include "Task.hpp"
+#include <iodrivers_base/ConfigureGuard.hpp>
+#include <sonar_oculus_m750d/Driver.hpp>
 
 using namespace sonar_oculus_m750d;
+using namespace iodrivers_base;
 
-Task::Task(std::string const& name)
+sonar_oculus_m750d::Task::Task(std::string const& name)
     : TaskBase(name)
 {
 }
 
-Task::~Task()
+sonar_oculus_m750d::Task::~Task()
 {
 }
 
-
-
-/// The following lines are template definitions for the various state machine
-// hooks defined by Orocos::RTT. See Task.hpp for more detailed
-// documentation about them.
-
-bool Task::configureHook()
+bool sonar_oculus_m750d::Task::configureHook()
 {
-    if (! TaskBase::configureHook())
+    auto beam_width = _beam_width.get();
+    auto beam_height = _beam_height.get();
+    std::unique_ptr<sonar_oculus_m750d::Driver> driver(
+        new sonar_oculus_m750d::Driver(beam_width, beam_height));
+    iodrivers_base::ConfigureGuard guard(this);
+    if (!_io_port.get().empty())
+        driver->openURI(_io_port.get());
+    setDriver(driver.get());
+    if (!TaskBase::configureHook()) {
         return false;
+    }
+    guard.commit();
+    m_fire_config = _fire_configuration.get();
     return true;
 }
-bool Task::startHook()
+
+bool sonar_oculus_m750d::Task::startHook()
 {
-    if (! TaskBase::startHook())
+    if (!TaskBase::startHook())
         return false;
+    sonar_oculus_m750d::Driver* driver =
+        static_cast<sonar_oculus_m750d::Driver*>(mDriver);
+    driver->fireSonar(m_fire_config);
     return true;
 }
-void Task::updateHook()
+
+void sonar_oculus_m750d::Task::updateHook()
 {
     TaskBase::updateHook();
 }
-void Task::errorHook()
+
+void sonar_oculus_m750d::Task::errorHook()
 {
     TaskBase::errorHook();
 }
-void Task::stopHook()
+
+void sonar_oculus_m750d::Task::stopHook()
 {
     TaskBase::stopHook();
 }
-void Task::cleanupHook()
+
+void sonar_oculus_m750d::Task::cleanupHook()
 {
     TaskBase::cleanupHook();
+}
+
+void sonar_oculus_m750d::Task::processIO()
+{
+    sonar_oculus_m750d::Driver* driver =
+        static_cast<sonar_oculus_m750d::Driver*>(mDriver);
+    auto sonar = driver->processOne();
+    if (sonar) {
+        _sonar.write(*sonar);
+    }
+    driver->fireSonar(m_fire_config);
 }
