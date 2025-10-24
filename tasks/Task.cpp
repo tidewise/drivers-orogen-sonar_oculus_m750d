1fd0e25fe7b5f84964f38f657db7563867522823
Wellington Castro <wellington.castro@tidewise.io>
2025-10-24 15:48:08 -0300

    feat: pressure safety check
    if safe_working_pressure is set to anything other than NaN
    the component will communicate with the hardware only when
    pressure input data is bigger than the safe_working_pressure
    value

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -9,6 +9,8 @@ using namespace sonar_oculus_m750d;
 Task::Task(std::string const& name)
     : TaskBase(name)
 {
+    _safe_working_pressure.set(base::Pressure::fromPascal(120000));
+    _pressure_data_timeout.set(base::Time::fromMilliseconds(300));
 }
 
 Task::~Task()
@@ -32,6 +34,8 @@ bool Task::configureHook()
     m_fire_config = _fire_configuration.get();
     m_driver = move(driver);
 
+    m_safe_working_pressure = _safe_working_pressure.get();
+    m_pressure_data_timeout_period = _pressure_data_timeout.get();
     return true;
 }
 
@@ -39,18 +43,71 @@ bool Task::startHook()
 {
     if (!TaskBase::startHook())
         return false;
-    m_driver->fireSonar(m_fire_config);
+
+    m_safe_to_work = base::isNaN(m_safe_working_pressure.toPa());
+    m_pressure_data_timeout = base::Timeout(m_pressure_data_timeout_period);
+
+    safeFireSonar();
+
     return true;
+}
+
+void Task::safeFireSonar()
+{
+    if (!m_safe_to_work) {
+        return;
+    }
+    m_driver->fireSonar(m_fire_config);
 }
 
 void Task::updateHook()
 {
     TaskBase::updateHook();
+
+    if (base::isNaN(m_safe_working_pressure.toPa())) {
+        return;
+    }
+
+    if(!m_safe_to_work && safeToWork()) { // unsafe -> safe transition
+        m_safe_to_work = true;
+        safeFireSonar();
+    }
+
+    m_safe_to_work = safeToWork();
+    if (!m_safe_to_work) {
+        return error(UNSAFE_WORKING_PRESSURE);
+    }
+}
+
+bool Task::safeToWork()
+{
+    base::samples::Pressure pressure;
+    auto flow = _pressure.read(pressure);
+    if (flow == RTT::NoData ||
+        (flow == RTT::OldData && m_pressure_data_timeout.elapsed())) {
+        return false;
+    }
+
+    if (flow == RTT::NewData) {
+        m_pressure_data_timeout.restart();
+    }
+
+    return pressure.toPa() > m_safe_working_pressure.toPa();
+}
+
+void Task::error(States error_state)
+{
+    if (state() != error_state) {
+        TaskBase::error(error_state);
+    }
 }
 
 void Task::errorHook()
 {
     TaskBase::errorHook();
+    if (state() == UNSAFE_WORKING_PRESSURE && safeToWork()) {
+        recover();
+    }
 }
 
 void Task::stopHook()
@@ -66,9 +123,8 @@ void Task::cleanupHook()
 void Task::processIO()
 {
     auto sonar = m_driver->processOne();
-
     if (sonar) {
         _sonar.write(*sonar);
-        m_driver->fireSonar(m_fire_config);
+        safeFireSonar();
     }
 }
